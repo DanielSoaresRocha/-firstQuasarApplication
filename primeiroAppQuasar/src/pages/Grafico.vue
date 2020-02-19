b38cb71b1132c31b73b521482f31fdbed5792f78
DanielSoaresRocha <legoliga123@gmail.com>
2020-02-19 15:35:34 -0300

    graficos com multiplas linhas

--- primeiroAppQuasar/src/pages/Grafico.vue
+++ primeiroAppQuasar/src/pages/Grafico.vue
@@ -1,5 +1,5 @@
 <template>
-    <pie-chart :data="[['Blueberry', 44], ['Strawberry', 23]]"></pie-chart>
+    <line-chart :data="data" />
 </template>
 
 <script>
@@ -10,7 +10,15 @@ import Chart from 'chart.js'
 Vue.use(Chartkick.use(Chart))
 
 export default {
-  name: 'grafico'
+  name: 'grafico',
+  data () {
+    return {
+      url: 'https://img.freepik.com/vetores-gratis/fundo-de-particulas-abstratas-tecnologia_52683-25766.jpg?size=626&ext=jpg',
+      model: '',
+      password: '',
+      isPwd: true,
+      data: [{ name: 'Workout', data: { '2017-01-01 00:00:00 -0800': 3, '2017-01-02 00:00:00 -0800': 4 } }, { name: 'Call parents', data: { '2017-01-01 00:00:00 -0800': 5, '2017-01-02 00:00:00 -0800': 3 } }]
+    }
+  }
 }
-
 </script>
